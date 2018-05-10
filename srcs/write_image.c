1ce21407be9f87b4f04a7bd0ee9201658ade0580
Abdelkrim BENHAMMOU <abbenham@e2r9p14.42.fr>
2018-05-10 15:37:31 +0200

    try split bresenham

--- srcs/write_image.c
+++ srcs/write_image.c
@@ -6,7 +6,7 @@
 /*   By: abbenham <newcratie@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/13 16:27:38 by abbenham          #+#    #+#             */
-/*   Updated: 2018/03/15 07:12:16 by abbenham         ###   ########.fr       */
+/*   Updated: 2018/05/10 15:33:29 by abbenham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,60 +23,44 @@ void		put_pixel(t_mlx *map, int x, int y, int color)
 	}
 }
 
-static void		put_vertical(t_line *ln, t_mlx *map, void (*f)(t_mlx *, int, int, int))
-{
-	int	lx;
-	int	ly;
-	int	x;
-	int	y;
-	int	cumul;
-
-	x = ln->xa;
-	y = ln->ya;
-	lx = ln->xb - ln->xa;
-	ly = ln->yb - ln->ya;
-	f(map, x, y, ln->color);
-	cumul = lx / 2;
-	y = ln->ya + 1;
-	while (y < ln->yb)
-	{
-		cumul += lx;
-		if (cumul >= ly)
-		{
-			cumul -= ly;
-			x += 1;
-		}
-		f(map, x, y++, ln->color);
-	}
-}
-
 void		put_line(t_line *ln, t_mlx *map, void (*f)(t_mlx *, int, int, int))
 {
-	int dx, dy, i, e;
-	int incx, incy, inc1, inc2;
-	int x,y;
+	int dx;
+    int	dy;
+	int i;
+	int e;
+	int incx;
+	int incy;
+	int inc1;
+	int inc2;
+	int x;
+	int y;
 
 	dx = ln->xb - ln->xa;
 	dy = ln->yb - ln->ya;
 
-	if(dx < 0) dx = -dx;
-	if(dy < 0) dy = -dy;
+	if (dx < 0)
+		dx = -dx;
+	if (dy < 0)
+		dy = -dy;
 	incx = 1;
-	if(ln->xb < ln->xa) incx = -1;
+	if (ln->xb < ln->xa)
+		incx = -1;
 	incy = 1;
-	if(ln->yb < ln->ya) incy = -1;
+	if (ln->yb < ln->ya)
+		incy = -1;
 	x=ln->xa;
 	y=ln->ya;
 
-	if(dx > dy)
+	if (dx > dy)
 	{
 		f(map, x, y, ln->color);
-		e = 2*dy - dx;
-		inc1 = 2*( dy -dx);
-		inc2 = 2*dy;
-		for(i = 0; i < dx; i++)
+		e = 2 * dy - dx;
+		inc1 = 2 * ( dy -dx);
+		inc2 = 2 * dy;
+		for (i = 0; i < dx; i++)
 		{
-			if(e >= 0)
+			if (e >= 0)
 			{
 				y += incy;
 				e += inc1;
@@ -89,12 +73,12 @@ void		put_line(t_line *ln, t_mlx *map, void (*f)(t_mlx *, int, int, int))
 	else
 	{
 		f(map, x, y, ln->color);
-		e = 2*dx - dy;
-		inc1 = 2*( dx - dy);
-		inc2 = 2*dx;
-		for(i = 0; i < dy; i++)
+		e = 2 * dx - dy;
+		inc1 = 2 * (dx - dy);
+		inc2 = 2 * dx;
+		for (i = 0; i < dy; i++)
 		{
-			if(e >= 0)
+			if (e >= 0)
 			{
 				x += incx;
 				e += inc1;
@@ -105,5 +89,3 @@ void		put_line(t_line *ln, t_mlx *map, void (*f)(t_mlx *, int, int, int))
 		}
 	}
 }
-
-
